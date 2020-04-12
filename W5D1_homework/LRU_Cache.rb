363638b5cb473275dfbacb6b1dc435d3d0383976
andy <jia470079834@gmail.com>
2020-04-12 18:38:22 -0400

    W5D1 homework

---
+++ W5D1_homework/LRU_Cache.rb
@@ -0,0 +1,30 @@
+class LRUCache
+    def initialize(size)
+        @size = size
+        @cache = []
+    end
+
+    def count
+        @cache.size
+    end
+
+    def add(el)
+        if self.count >= @size
+            @cache.shift
+            @cache << el
+        elsif @cache.include?(el)
+            @cache.delete(el)
+            @cache << el
+        else
+            @cache << el
+        end
+    end
+
+    def show
+        p @cache
+        nil
+
+    end
+
+    private
+end
