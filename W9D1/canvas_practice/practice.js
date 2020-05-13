b673bcccea61d8963210666e125decf42afd29a0
andy <jia470079834@gmail.com>
2020-05-13 00:46:09 -0400

    W9D3 HW

---
+++ W9D1/canvas_practice/practice.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const canvasEl = document.getElementById("mycanvas");
+    canvasEl.width = 500;
+    canvasEl.height = 500;
+
+    const ctx = canvasEl.getContext("2d");
+    ctx.fillStyle = "purple";
+    ctx.fillRect(10, 10, 500, 500);
+
+    ctx.beginPath();
+    ctx.arc(100, 100, 20, 0, 2 * Math.PI, true);
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.fillStyle = "blue";
+    ctx.fill();
+});
+
+
